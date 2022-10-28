6616b136c8f5bbaf03f0fd1e82f6520abea56657
key muha <91820369+keytosucces@users.noreply.github.com>
2022-10-28 16:41:32 -0700

    Add files via upload

---
+++ fi-nd-me-dia-n.js
@@ -0,0 +1,12 @@
+function keymedian(arr) {
+
+    arr.sort( function(x , y) {return x - y;} );
+
+    const middle = Math.floor(arr.length/2);
+
+    if (arr.length % 2)
+        return arr[middle];
+    else
+        return (arr[middle-1] + arr[middle]) / 2 ;
+
+}
