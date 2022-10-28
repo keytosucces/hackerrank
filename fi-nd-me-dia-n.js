b3de502b24a4966a5f12ac0aab2330c8bd1a8502
key muha <91820369+keytosucces@users.noreply.github.com>
2022-10-28 16:38:14 -0700

    Add files via upload

---
+++ fi-nd-me-dia-n.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'findMedian' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function findMedian(arr) {
+
+    arr.sort( function(x , y) {return x - y;} );
+
+    var middle = Math.floor(arr.length/2);
+
+    if(arr.length % 2)
+        return arr[middle];
+    else
+        return (arr[middle-1] + arr[middle]) / 2 ;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = findMedian(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
