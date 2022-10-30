3f987d1daeabecebcaead4fcaaef2ed01b2f36dc
key muha <91820369+keytosucces@users.noreply.github.com>
2022-10-29 22:28:48 -0700

    Delete pl-us-m-i--n-u-s.js

--- pl-us-m-i--n-u-s.js
+++
@@ -1,8 +0,0 @@
-function keyPlusMinus(arr) {
-    
-    let pos = arr.filter(number => number > 0).length / arr.length;
-    let neg = arr.filter(number => number < 0).length / arr.length;
-    let nonum = arr.filter(number => number === 0).length / arr.length;
-
-    return console.log(pos.toFixed(6) + neg.toFixed(6) + nonum.toFixed(6));
-}
